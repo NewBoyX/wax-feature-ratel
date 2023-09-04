4dbb6ac58d899b1363bfad8466987d3b90df7fa6
“WaxVerbena” <necg2009@163.com>
2023-09-04 23:44:08 +0800

    +swagger

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-# SkyWalking Agent 配置
-export SW_AGENT_NAME=wax-app # 配置 Agent 名字。一般来说，我们直接使用 Spring Boot 项目的 `spring.application.name` 。
-export SW_AGENT_COLLECTOR_BACKEND_SERVICES=1.14.245.144:11800 # 配置 Collector 地址。
-export SW_AGENT_SPAN_LIMIT=2000 # 配置链路的最大 Span 数量。一般情况下，不需要配置，默认为 300 。主要考虑，有些新上 SkyWalking Agent 的项目，代码可能比较糟糕。
-export JAVA_AGENT=-javaagent:skywalking-agent.jar # SkyWalking Agent jar 地址。
 
 # Jar 启动
-java -jar $JAVA_AGENT -jar    /Users/javafx/github/wax/target/wax-app-0.0.1-SNAPSHOT.jar
+java -jar  wax-app-0.0.1-SNAPSHOT.jar
