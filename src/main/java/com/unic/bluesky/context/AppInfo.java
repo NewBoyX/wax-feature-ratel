a736540153ba8fac98a721972bc2f49925ed3f6d
“WaxVerbena” <necg2009@163.com>
2023-09-10 14:18:00 +0800

    +swagger

---
+++ src/main/java/com/unic/bluesky/context/AppInfo.java
@@ -0,0 +1,14 @@
+package com.unic.bluesky.context;
+
+import com.unic.bluesky.business.user.vo.UserVo;
+
+/**
+ * 应用信息上下文
+ */
+public class AppInfo {
+
+
+    public static UserVo getCurrentUser() {
+        return null;
+    }
+}
